40512536e264e641084e4ddebbb5dbd6ab962aef
Ahmed Sakka <ahmedsakka720@gmail.com>
2021-01-01 12:35:22 +0100

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleFormComponent } from './article-form/article-form.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EventFormComponent } from './event-form/event-form.component';
+import { EventListComponent } from './event-list/event-list.component';
 import { MemberFormComponent } from './member-form/member-form.component';
 import {MemberListComponent} from './member-list/member-list.component';
+import { ToolFormComponent } from './tool-form/tool-form.component';
+import { ToolListComponent } from './tool-list/tool-list.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'members'
+    component: DashboardComponent,
   },
   {
     path: 'members',
@@ -34,8 +41,80 @@ const routes: Routes = [
     ]
   },
   {
+    path: 'articles',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ArticleListComponent,
+      },
+      {
+        path: 'create',
+        pathMatch: 'full',
+        component: ArticleFormComponent,
+      },
+      {
+        path: ':id/edit',
+        pathMatch: 'full',
+        component: ArticleFormComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      },
+    ]
+  },
+  {
+    path: 'events',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: EventListComponent,
+      },
+      {
+        path: 'create',
+        pathMatch: 'full',
+        component: EventFormComponent,
+      },
+      {
+        path: ':id/edit',
+        pathMatch: 'full',
+        component: EventFormComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      },
+    ]
+  },
+  {
+    path: 'tools',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ToolListComponent,
+      },
+      {
+        path: 'create',
+        pathMatch: 'full',
+        component: ToolFormComponent,
+      },
+      {
+        path: ':id/edit',
+        pathMatch: 'full',
+        component: ToolFormComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      },
+    ]
+  },
+  {
     path: '**',
-    redirectTo: 'members',
+    redirectTo: '',
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MemberFormComponent } from './member-form/member-form.component';
 import { SharedModule } from '../@root/shared.module';
 import { LayoutComponent } from './layout/layout.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { EventFormComponent } from './event-form/event-form.component';
+import { ArticleFormComponent } from './article-form/article-form.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ToolListComponent } from './tool-list/tool-list.component';
+import { ToolFormComponent } from './tool-form/tool-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -16,6 +23,13 @@ import { LayoutComponent } from './layout/layout.component';
     MemberListComponent,
     MemberFormComponent,
     LayoutComponent,
+    EventListComponent,
+    EventFormComponent,
+    ArticleFormComponent,
+    ArticleListComponent,
+    ToolListComponent,
+    ToolFormComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
